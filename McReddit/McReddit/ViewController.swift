2f66cbdafdba92688028a8b934ed5de40653a9bb
Thomas Ricouard <ricouard77@gmail.com>
2018-12-23 17:19:28 +0100

    Initial architecture

---
+++ McReddit/McReddit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  McReddit
+//
+//  Created by Thomas Ricouard on 23/12/2018.
+//  Copyright © 2018 Thomas Ricouard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
